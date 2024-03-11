d03f8c9ef49d54dbd974483aff26307e2d2903b8
María Julieta Comparan Velasco <91906153+engineermj98@users.noreply.github.com>
2024-03-11 10:59:49 -0600

    Add files via upload

--- RickAndMorty/wwwroot/js/site.js
+++ RickAndMorty/wwwroot/js/site.js
@@ -1,5 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-
+﻿
 // Espera a que el DOM esté cargado
 document.addEventListener("DOMContentLoaded", function () {
     // Obtén todos los elementos con la clase "character-name"
@@ -18,5 +17,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 details.style.display = "none";
             }
         });
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = new Audio("extra/RYMSoundtrack.mp3");
+    audio.loop = true;
+
+    // Función para reproducir el audio cuando el usuario hace clic en un botón
+    function playAudio() {
+        var playPromise = audio.play();
+
+        if (playPromise !== undefined) {
+            playPromise.then(_ => {
+                // La reproducción comenzó correctamente
+            }).catch(error => {
+                // El usuario denegó el permiso, manejarlo aquí
+                console.error("Error al reproducir audio:", error);
+            });
+        }
+    }
+
+    // Asociar la función playAudio al evento clic de un botón
+    var playButton = document.getElementById("playButton");
+    if (playButton) {
+        playButton.addEventListener("click", playAudio);
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = document.getElementById("audioPlayer");
+    audio.play().catch(function (error) {
+        console.error("Error al reproducir audio:", error);
     });
 });
