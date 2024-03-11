8a6c31d68810ba80154e4cb46dd29f951457bfe1
María Julieta Comparan Velasco <91906153+engineermj98@users.noreply.github.com>
2024-03-11 01:14:16 -0600

    Add files via upload

---
+++ RickAndMorty/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+
+// Espera a que el DOM esté cargado
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén todos los elementos con la clase "character-name"
+    var characterNames = document.querySelectorAll(".character-image");
+
+    // Itera sobre cada elemento y agrega un eventListener de clic
+    characterNames.forEach(function (element) {
+        element.addEventListener("click", function () {
+            // Encuentra el contenedor de detalles correspondiente
+            var details = this.parentElement.querySelector(".character-details");
+
+            // Muestra u oculta los detalles alternando la visibilidad
+            if (details.style.display === "none") {
+                details.style.display = "block";
+            } else {
+                details.style.display = "none";
+            }
+        });
+    });
+});
