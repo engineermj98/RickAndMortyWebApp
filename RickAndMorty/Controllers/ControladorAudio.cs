426f3a3b8c1f41273e3e39dec0d13bf9d4651dce
María Julieta Comparan Velasco <91906153+engineermj98@users.noreply.github.com>
2024-03-11 09:08:47 -0600

    Add files via upload

---
+++ RickAndMorty/Controllers/ControladorAudio.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RickAndMorty.Controllers
+{
+    public class ControladorAudio : Controller
+    {
+        public IActionResult PlayAudioLoop()
+        {
+            // Lógica del controlador
+
+            // Devolver JavaScript para reproducir el audio en un ciclo infinito
+            return Content("<script>playAudioLoop();</script>", "text/html");
+        }
+    }
+}
